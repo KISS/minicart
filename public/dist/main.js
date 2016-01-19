f4cad7590848f255728f233e493011cd9fa979d2
Luisa Morales <me@luisam.com>
2016-01-18 20:32:17 -0500

    Added normalize file

--- public/dist/main.js
+++ public/dist/main.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
 	var products = $.ajax( "/products" )
 		.done(function(data) {
 			$("#product-listing").html(productTemplate({ products: data } ));
-			console.log( "success-prod", data, data[0] );
 		})
 		.fail(function() {
 			console.log( "error-cart" );
@@ -18,7 +17,6 @@ $(document).ready(function(){
 	var cart = $.ajax( "/cart_order" )
 		.done(function(data) {
 			$("#product-cart").html(cartTemplate({ cart_order: data } ));
-			console.log( "success-cart", data );
 		})
 		.fail(function() {
 			console.log( "error-cart" );
